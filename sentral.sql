c6095baa75f0494f01cfe7d543e8a7388ee0373e
agutri01 <73772214+agutri01@users.noreply.github.com>
2020-11-01 15:34:53 +0700

    Add files via upload

---
+++ sentral.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.37-MariaDB : Database - sentral
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sentral` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sentral`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `kode_user` char(12) NOT NULL,
+  `nama_user` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telp` varchar(13) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`kode_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`kode_user`,`nama_user`,`email`,`telp`,`alamat`,`password`) values ('PL00000001','agus','agustriprawira01@gmail.com','081234567890','jl.tanmalaka','827ccb0eea8a706c4c34a16891f84e7b');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
